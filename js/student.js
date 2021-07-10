7a74fb20dc59cd7a4e431f1fe1fff1c3747d54d6
mahdy hisham <mahdyhisham777@gmail.com>
2021-07-10 06:05:31 +0200

    modifiy student section

---
+++ js/student.js
@@ -0,0 +1,84 @@
+// scroll to top Button
+let scrtop = document.getElementById("scrollto");
+let departments = document.querySelectorAll(".dep_name");
+function btn() {
+  window.onscroll = () => {
+    if (window.pageYOffset >= 100) {
+      scrtop.style.display = "block";
+    } else {
+      scrtop.style.display = "none";
+    }
+
+    // department section
+    // department section
+  };
+  scrtop.onclick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+}
+btn();
+// start header toggle
+
+//
+
+// can not hidde menu without toggel button
+var menuTogg = document.querySelector(".bars"),
+  navg = document.querySelector(".nav"),
+  list = document.querySelector(".list");
+
+function toggleMenu() {
+  menuTogg.addEventListener("click", (e) => {
+    navg.classList.toggle("menu");
+    list.classList.toggle("show");
+  });
+  window.onresize = () => {
+    let pageWidth = window.innerWidth;
+    if (pageWidth > 767) {
+      navg.classList.remove("menu");
+      list.classList.remove("show");
+    }
+  };
+}
+toggleMenu();
+// End header toggle
+
+// footer list-show-hide
+
+//want to remove active when click any wher eles
+let plus = document.querySelectorAll(".H-S");
+let linkList = document.querySelectorAll(".listlinks");
+function togglebtn(pagin, list, attr) {
+  pagin.forEach((plu, i) => {
+    plu.addEventListener("click", (e) => {
+      list.forEach((lis) => {
+        lis.classList.remove(`${attr}`);
+      });
+      // can not close it again onclick twice
+      list[i].classList.toggle(`${attr}`);
+    });
+  });
+}
+togglebtn(plus, linkList, `active`);
+
+//footer list-show-hide
+
+// giude btn shown content
+
+let guide = document.querySelectorAll(".guidebtn");
+let a = document.querySelectorAll(".a");
+guide.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    departments.forEach((dep, j) => {
+      a[j].classList.remove("act_btn");
+      dep.classList.remove("show");
+    });
+    btn.classList.add("act_btn");
+    departments[i].classList.add("show");
+  });
+});
+
+console.log(departments);
+console.log(a);
