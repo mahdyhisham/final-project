4f2e133d2d2ebfcc57e68e8cbf801a59f16d67d7
Sekab <ataher@inmobly.com>
2021-07-12 02:20:56 +0200

    add firebase to contactus and student views

--- js/student.js
+++ js/student.js
@@ -82,3 +82,68 @@ guide.forEach((btn, i) => {
 
 console.log(departments);
 console.log(a);
+
+function getStudentResults(e) {
+// this is the function that does the following:
+// - Reads the form data (sitting number / name / year / department and etc..)  
+// - Reads the student document based on his sitting number from firestore database
+// - If there are results it will call another function and pass the data to it called showData(data,year)
+// - If data is invalid or nothing comes from firestore it will alert an error
+
+  const name = document.querySelector('input#name').value;
+  const yearSelector = document.querySelector('select#year');
+  const year = +yearSelector.options[yearSelector.selectedIndex].text;
+  const sittingNumber = +document.querySelector('input#sittingNumber').value;
+
+  const departmentSelector = document.querySelector('select#department');
+  const department = +departmentSelector.options[departmentSelector.selectedIndex].text
+
+
+  const studentRef = db.collection('students').doc(String(sittingNumber));
+
+  studentRef.get().then(resp => {
+    const data = resp.data();
+    if (data) {
+      showData(data, year);
+    } else {
+      alert('Could not find your results with the provided ID');
+    }
+
+  }).catch(err => {
+    alert('Could not find your results with the provided ID');
+  })
+  document.querySelector('form').reset();
+  return false;
+}
+
+function showData(data, year) {
+// this is the function that does the following:
+// - Creates the view for the results 
+// - Add text to the header showing the department name
+// - Adds the rest of the view (table cells, table data and so on)
+
+  try {
+    document.querySelector('h1.hidden-h1').style.display = 'block'
+    const results = data.years[year].firstSemseter;
+    const resultsList = document.querySelector('.results-list');
+
+    document.querySelector('h1.department-name').textContent = data.department;
+
+    Object.keys(results).forEach(k => {
+      const li = document.createElement("li");
+      const subjectElm = document.createElement("span");
+      subjectElm.textContent = k;
+      const scoreElm = document.createElement("span");
+      scoreElm.textContent = results[k];
+      li.appendChild(subjectElm);
+      li.appendChild(document.createTextNode(" : "));
+      li.appendChild(scoreElm);
+      resultsList.appendChild(li);
+    })
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+function showError(err) {
+}
